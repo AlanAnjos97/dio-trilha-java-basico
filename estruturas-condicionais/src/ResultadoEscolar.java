12710774b18e2485ec73a3264432905702211e4b
AlanAnjos <eualananjos@gmail.com>
2024-09-30 05:43:07 -0300

    Adicionado Resultado Escolar com controlador ternário

--- estruturas-condicionais/src/ResultadoEscolar.java
+++ estruturas-condicionais/src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        double nota = 6.5;
+        double nota = 4.9;
         double media = 7;
+        
         if (nota >= media) {
             System.out.println("Aprovado");
+        } else if (nota >= 5 && nota < media) {
+            System.out.println("Recuperação");
         } else {
             System.out.println("Reprovado");
         }
